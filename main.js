2bcfbbc3975e0b4710fbf654a09f8af32da3d68b
ELDADBRHANO <ebrhano@gmail.com>
2022-06-15 14:08:45 +0300

    commit

---
+++ main.js
@@ -0,0 +1,90 @@
+// function printRegards(){
+//   let inputId = +document.getElementById("input_id").value;
+//   switch (inputId) {
+//     case 3 :
+//       console.log("hello");
+//       inputId = ""
+//       break;
+
+//     case 5 :
+//     console.log("bye");
+//     inputId = ""
+//     break;
+
+//     case 7 :
+//       console.log("tnx");
+//       inputId = ""
+//       break;
+
+//     default:
+//       console.log("number is not exist");
+//       inputId = ""
+//       break;
+//   }
+
+// }
+
+// // 2
+// function getUserName() {
+//   let inputId = document.getElementById("input_id").value;
+//   switch(inputId){
+//     case "jacob":
+//       console.log(inputId);
+//       return;
+//       case "nathan":
+//         h2_id.innerHTML =inputId.toUpperCase();
+//         return;
+//         case "DALYA":
+//           h2_id.innerHTML =inputId.toULowerCase();
+
+//           return;
+//           default:
+//             h2_id.innerHTML ="nothing"
+
+//   }
+// }
+
+// 3
+// function printUserNumbersAndSigns() {
+// switch(input_id3.value){
+// case "+":
+//   h2_id.innerHTML = +input_id.value +  +input_id2.value;
+//   return;
+// case "-":
+//   h2_id.innerHTML = +input_id.value +  +input_id2.value;
+//   return;
+// case "*":
+//   h2_id.innerHTML = +input_id.value +  +input_id2.value;
+// return;
+// case "/":
+//   h2_id.innerHTML = +input_id.value +  +input_id2.value;
+//   return;
+// default:
+//   h2_id.innerHTML = "none";
+//   return;
+// }
+// }
+
+// 4
+function printColor() {
+  switch (corner.value) {
+    case "top-left":
+      div_id.style = "width:100px; height:200px; top:40px; border-radius:75%; background:"+input_id2.value;
+      break;
+    case "top-right":
+      div_id.style = "width:100px; height:200px; display:flex; float:right; top:40px; border-radius:75%; background:"+input_id2.value;
+
+      break;
+    case "botoom-left":
+      div_id.style = "width:100px; height:200px; top:40px; border-radius:75%; background:"+input_id2.value;
+
+      break;
+    case "botoom-right":
+      div_id.style = "width:100px; height:200px; top:40px; border-radius:75%; background:"+input_id2.value;
+
+      break;
+    default:
+      div_id.style = "enter somthing"
+      break;
+  }
+}
